5a5d6d8461a0f672c2fa70b67682e25459e21c45
Gabriel Barros de Almeida <g.barros.de.almeida@avanade.com>
2025-02-06 17:05:43 -0300

    Added jdbcTemplate
    Signed-off-by: Gabriel Barros de Almeida <g.barros.de.almeida@avanade.com>

--- src/main/java/com/pluralsight/conference/repository/SpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/conference/repository/SpeakerRepositoryImpl.java
@@ -1,13 +1,21 @@
 package com.pluralsight.conference.repository;
 
 import com.pluralsight.conference.model.Speaker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Repository("speakerRepository")
 public class SpeakerRepositoryImpl implements SpeakerRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public SpeakerRepositoryImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     public List<Speaker> findAll() {
         Speaker speaker = new Speaker();
@@ -16,5 +24,29 @@ public class SpeakerRepositoryImpl implements SpeakerRepository {
         List<Speaker> speakers = new ArrayList<>();
         speakers.add(speaker);
         return speakers;
+    }
+
+    @Override
+    public Speaker create(Speaker speaker) {
+
+        jdbcTemplate.update("INSERT INTO speaker (name) VALUES (?)", speaker.getName());
+
+//        SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate);
+//
+//        insert.setTableName("speaker");
+//
+//        List<String> columns = new ArrayList<>();
+//        columns.add("name");
+//
+//        Map<String, Object> data = new HashMap<>();
+//        data.put("name", speaker.getName());
+//
+//        insert.setGeneratedKeyName("id");
+//
+//        Number key = insert.executeAndReturnKey(data);
+//
+//        System.out.println(key);
+
+        return null;
     }
 }
